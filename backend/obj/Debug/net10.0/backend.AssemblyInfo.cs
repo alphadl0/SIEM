9bce19066cc82c4b4c0b364511d5b279d45051f9
Alpha Diallo <boydiallo829@gmail.com>
2026-04-09 11:50:03 +0300

    cleaned up filter and search and nav bar

--- backend/obj/Debug/net10.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net10.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8af4158005c648f8d603ef8d7bd5565e8a5be1f7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2559deb1a4500030aaf4dd7a8b8acebcf1d8871a")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
